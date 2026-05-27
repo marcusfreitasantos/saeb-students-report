4e2f34d4f46d9a2e073e69e09968f6c880d80d9a
Marcus Freitas <marcusfreitasantos@gmail.com>
2026-05-26 22:58:55 -0300

    feat: generate presigned url lambda

---
+++ src/functions/generate-presigned-url/application/bucket_usecase.py
@@ -0,0 +1,17 @@
+import uuid
+from datetime import date, datetime
+
+class BucketUseCase:
+    def __init__(self, repository):
+        self.repository = repository
+
+    def get_signed_url(self):
+        now = datetime.now()
+        day = date.today().day
+        month = now.month
+        year = date.today().year
+
+        key = f"input-files/{year}/{month}/{day}/{uuid.uuid4()}/students-data"
+        signed_url = self.repository.presigned_url(key)
+        return signed_url
+    
